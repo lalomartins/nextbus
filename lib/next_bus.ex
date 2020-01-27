9d981ea23e7efc56843e4fd9df720bda5b156169
Lalo Martins <lalo.martins@gmail.com>
2020-01-27 20:02:49 +0200

    add some tests

--- lib/next_bus.ex
+++ lib/next_bus.ex
@@ -1,31 +1,29 @@
 defmodule NextBus do
   @moduledoc """
-  Documentation for NextBus.
+  Get and parse schedules from Tallinn transport authority
   """
 
-  @doc """
-  Hello world.
+  @type departure :: %{
+    transport: String.t,
+    route: String.t,
+    expected: integer,
+    scheduled: integer,
+  }
 
-  ## Examples
-
-      iex> NextBus.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
-  @spec get_schedule([stop: String.t, route: String.t]) :: any
+  @spec get_schedule([stop: String.t, route: String.t]) :: [departure]
   def get_schedule([stop: stop, route: route]) do
     {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body}} =
       :httpc.request(:get, {'https://transport.tallinn.ee/siri-stop-departures.php?stopid=#{stop}&time=', []}, [], [])
-    {:ok, sio} = body |> to_string |> StringIO.open
-    departures = sio
-    |> IO.binstream(:line)
-    |> CSV.decode(validate_row_length: false, headers: true)
-    |> get_departures(route)
-    departures
+    body |> to_string |> parse_schedule(route)
+  end
+
+  @spec parse_schedule(String.t, String.t) :: [departure]
+  def parse_schedule(schedule, route) do
+      {:ok, sio} = schedule |> StringIO.open
+      sio
+      |> IO.binstream(:line)
+      |> CSV.decode(validate_row_length: false, headers: true)
+      |> get_departures(route)
   end
 
   defp get_departures(csv_data, nil) do
@@ -34,8 +32,8 @@ defmodule NextBus do
     |> Enum.map(fn({:ok, row}) -> %{
       transport: row["Transport"],
       route: row["RouteNum"],
-      expected: row["ExpectedTimeInSeconds"],
-      scheduled: row["ScheduleTimeInSeconds"],
+      expected: String.to_integer(row["ExpectedTimeInSeconds"]),
+      scheduled: String.to_integer(row["ScheduleTimeInSeconds"]),
     } end)
   end
 
--- test/next_bus_test.exs
+++ test/next_bus_test.exs
@@ -2,7 +2,39 @@ defmodule NextBusTest do
   use ExUnit.Case
   doctest NextBus
 
-  test "greets the world" do
-    assert NextBus.hello() == :world
+  test "parse empty schedule" do
+    assert NextBus.parse_schedule("", nil) == []
+  end
+
+  test "parse valid schedule" do
+    schedule = NextBus.parse_schedule("""
+    Transport,RouteNum,ExpectedTimeInSeconds,ScheduleTimeInSeconds
+    stop,13199
+    bus,39,72716,72716,Veerenni
+    bus,39,74276,74276,Veerenni
+    """, nil)
+    assert length(schedule) == 2
+    [first, second] = schedule
+    assert first[:transport] == "bus"
+    assert second[:transport] == "bus"
+    assert first[:expected] == 72716
+  end
+
+  test "filter schedule by route" do
+    schedule = NextBus.parse_schedule("""
+    Transport,RouteNum,ExpectedTimeInSeconds,ScheduleTimeInSeconds
+    stop,13199
+    bus,39,72716,72716,Veerenni
+    bus,23,72256,72256,Liikuri
+    bus,23,73816,73816,Liikuri
+    bus,39,74276,74276,Veerenni
+    bus,23,75376,75376,Liikuri
+    """, "39")
+    assert length(schedule) == 2
+    [first, second] = schedule
+    assert first[:transport] == "bus"
+    assert second[:transport] == "bus"
+    assert first[:expected] == 72716
+    assert second[:route] == "39"
   end
 end
